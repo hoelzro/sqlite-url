866c23afc292e0198bc96f86a4267c62ed8f148e
Rob Hoelz <rob@hoelz.ro>
2020-03-15 13:05:26 -0500

    Add simple url_user test

--- test.sql
+++ test.sql
@@ -2,6 +2,9 @@
 .selftest --init
 
 insert into selftest (op, cmd, ans) values
+    ('memo', 'url_user tests', ''),
+    ('run', "select url_user('http://example.com')", ''),
+    ('memo', 'url_scheme tests', ''),
     ('run', "select url_scheme('http://example.com')", 'http'),
     ('run', "select url_scheme('https://example.com')", 'https');
 
